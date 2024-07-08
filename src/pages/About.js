5be65cf620d0c72e1e99edb276638efa4f51cb70
Endriyas <endriyas@uni.minerva.edu>
2024-07-09 01:39:08 +0300

    about-error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
+    <div className="section about-section">
+      <h2 className="section-title">about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
+        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
+        hic aspernatur error blanditiis?
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
